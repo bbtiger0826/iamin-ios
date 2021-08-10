a80073090077e66170818219824d7b027425f1c1
陳柏文 <bbtiger0826@gmail.com>
2021-08-10 16:29:10 +0800

    賣家 - 2020/08/10

--- iamin/iamin/Group/GroupCell.swift
+++ iamin/iamin/Group/GroupCell.swift
@@ -3,6 +3,7 @@ import UIKit
 
 class GroupCell: UITableViewCell {
     
+    @IBOutlet weak var imageGroup: UIImageView!
     @IBOutlet weak var lableName: UILabel!
     @IBOutlet weak var lableContactNumber: UILabel!
 }
